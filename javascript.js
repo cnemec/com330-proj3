fa601771081a1ec542caa8b77a4daea7fa2d35b8
cnemec <cnemec@hawk.iit.edu>
2018-11-05 18:00:03 -0600

    using guide on how to make an image gallery as a base

---
+++ javascript.js
@@ -0,0 +1,33 @@
+<script>
+  function openGal(){
+    document.getElementById('gallery').style.display="block";
+  }
+  function closeGal(){
+    document.getElementById('gallery').style.display="none";
+  }
+  var slider = 1;
+  showSlides(slider);
+  function moveSlides(n){
+    showSlides(slider+=n);
+  }
+  function currentSlide(n) {
+    showSlides(slider = n);
+}
+function showSlides(n) {
+var i;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("picture");
+var captionText = document.getElementById("caption");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+captionText.innerHTML = dots[slideIndex-1].alt;
+}
+</script>
