910919b8f4390451b55c15e8884985d7dc4527e9
cnemec <cnemec@hawk.iit.edu>
2018-11-28 19:17:41 -0600

    umm what?

--- javascript.js
+++ javascript.js
@@ -4,16 +4,16 @@ function opengal(images){
   if(images.src = "ibfanartmini.jpg"){
     expandimg.src = "ibfanart.jpg";
   }
-  if(images.src = "ibfanartmini2.jpg"){
+  else if(images.src = "ibfanartmini2.jpg"){
     expandimg.src = "ibfanart2.jpg";
   }
-  if(images.src = "ibfanartmini3.jpg"){
+  else if(images.src = "ibfanartmini3.jpg"){
     expandimg.src = "ibfanart3.jpg";
   }
-  if(images.src = "ibfanartmini4.jpg"){
+  else if(images.src = "ibfanartmini4.jpg"){
     expandimg.src = "ibfanart4.jpg";
   }
-  if(images.src = "ibfanartmini5.jpg"){
+  else if(images.src = "ibfanartmini5.jpg"){
     expandimg.src = "ibfanart5.jpg";
   }
   imgText.innerHTML = images.alt;
