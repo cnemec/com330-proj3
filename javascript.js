bf56eaf7048c8452bb52cfced5f88b74bcad2650
cnemec <cnemec@hawk.iit.edu>
2018-11-28 19:45:03 -0600

    feel like everything is there just need to make it appear when called

--- javascript.js
+++ javascript.js
@@ -1,19 +1,21 @@
 function opengal(images){
   var expandimg = document.getElementById("expandedImg");
   if(images.src == "ibfanartmini.jpg"){
-    expandimg.src = "ibfanart.jpg";
+    expandimg.src = "ibfanartmini.jpg";
   }
   else if(images.src == "ibfanartmini2.jpg"){
-    expandimg.src = "ibfanart2.jpg";
+    expandimg.src = "ibfanartmini2.jpg";
   }
   else if(images.src == "ibfanartmini3.jpg"){
-    expandimg.src = "ibfanart3.jpg";
+    expandimg.src = "ibfanartmini3.jpg";
   }
   else if(images.src == "ibfanartmini4.jpg"){
-    expandimg.src = "ibfanart4.jpg";
+    expandimg.src = "ibfanartmini4.jpg";
   }
   else if(images.src == "ibfanartmini5.jpg"){
-    expandimg.src = "ibfanart5.jpg";
+    expandimg.src = "ibfanartmini5.jpg";
   }
-  expandimg.parentElement.style.display = "block";
+  expandedImg.style.display = "block";
+  document.getElementById("galleryset").style.display = "block";
+
 }
