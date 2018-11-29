35d43c1002d51c42b4e49b10a529d01739609489
cnemec <cnemec@hawk.iit.edu>
2018-11-29 12:17:28 -0600

    kinda fixed it why aren't the images being called

--- javascript.js
+++ javascript.js
@@ -1,19 +1,21 @@
 function opengal(images){
   var expandimg = document.getElementById("expandedImg");
+    expandimg.src = images.src;
+    console.log(images.src);
   if(images.src == "ibfanartmini.jpg"){
-    expandimg.src = "ibfanartmini.jpg";
+    expandimg.src = "ibfanart.jpg";
   }
   else if(images.src == "ibfanartmini2.jpg"){
-    expandimg.src = "ibfanartmini2.jpg";
+    expandimg.src = "ibfanart2.jpg";
   }
   else if(images.src == "ibfanartmini3.jpg"){
-    expandimg.src = "ibfanartmini3.jpg";
+    expandimg.src = "ibfanart3.jpg";
   }
   else if(images.src == "ibfanartmini4.jpg"){
-    expandimg.src = "ibfanartmini4.jpg";
+    expandimg.src = "ibfanart4.jpg";
   }
   else if(images.src == "ibfanartmini5.jpg"){
-    expandimg.src = "ibfanartmini5.jpg";
+    expandimg.src = "ibfanart5.jpg";
   }
   expandedImg.style.display = "block";
   document.getElementById("galleryset").style.display = "block";
