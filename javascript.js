dd5077cfcb7d44e84504baa16e8f7e129ad8ec2b
cnemec <cnemec@hawk.iit.edu>
2018-11-28 19:12:06 -0600

    why is the og image disappearing and the same new one appearing?

--- javascript.js
+++ javascript.js
@@ -1,44 +1,21 @@
-(function(){
-  if (typeof document.querySelector === 'undefined') {
-    return; // be kind to old browsers and get out
+function opengal(images){
+  var expandimg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  if(images.src = "ibfanartmini.jpg"){
+    expandimg.src = "ibfanart.jpg";
   }
-  var slideshow = document.querySelector('.gallery');
-  // Replace the gallery class with slideshow
-  slideshow.className = 'slideshow';
-  var figcaption = slideshow.querySelector('figcaption');
-  figcaption.innerHTML += '<b>Click or tap image to see more images.</b>';
-  // Grab all the slides -- that is, the list items
-  var slides = slideshow.querySelectorAll('li');
-  console.log(slides.length, 'slides');
-
-  slides[0].className = 'is-showing';
-
-  // Create the next_slide stepper function from cycle(), defined below
-  var next_slide = cycle(slides);
-  var old_slide = slides[0];
-
-  slideshow.addEventListener('click', function(e){
-    old_slide.className = ''; // remove is-showing from old slide
-    var slide = next_slide();
-    slide.className = 'is-showing';
-    old_slide = slide;
-  })
-
-  // Function to cycle endlesslyl through an array;
-  function cycle(arr) {
-    var max_index = arr.length - 1;
-    var current_index = 0;
-    return function() {
-      // increment the index by 1
-      current_index++;
-      // if the index is bigger than 1, reset to 0
-      if (current_index > max_index) {
-        current_index = 0;
-      }
-      var item = arr[current_index];
-      return item;
-    }
+  if(images.src = "ibfanartmini2.jpg"){
+    expandimg.src = "ibfanart2.jpg";
   }
-
-
-})();
+  if(images.src = "ibfanartmini3.jpg"){
+    expandimg.src = "ibfanart3.jpg";
+  }
+  if(images.src = "ibfanartmini4.jpg"){
+    expandimg.src = "ibfanart4.jpg";
+  }
+  if(images.src = "ibfanartmini5.jpg"){
+    expandimg.src = "ibfanart5.jpg";
+  }
+  imgText.innerHTML = images.alt;
+  expandimg.parentElement.style.display = "block";
+}
