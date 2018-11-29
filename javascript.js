82b99e0181f0d88c39db31836e9eaf7da4f7fc06
cnemec <cnemec@hawk.iit.edu>
2018-11-29 12:48:18 -0600

    FINALLY

--- javascript.js
+++ javascript.js
@@ -1,20 +1,22 @@
 function opengal(images){
   var expandimg = document.getElementById("expandedImg");
     expandimg.src = images.src;
-    console.log(images.src);
-  if(images.src == "ibfanartmini.jpg"){
+    var fme = images.src.slice(-16);
+  if(fme == "ibfanartmini.jpg"){
     expandimg.src = "ibfanart.jpg";
   }
-  else if(images.src == "ibfanartmini2.jpg"){
+  var fme = images.src.slice(-17);
+  if(fme == "ibfanart2mini.jpg"){
     expandimg.src = "ibfanart2.jpg";
   }
-  else if(images.src == "ibfanartmini3.jpg"){
+  else if(fme == "ibfanar3tmini.jpg"){
     expandimg.src = "ibfanart3.jpg";
   }
-  else if(images.src == "ibfanartmini4.jpg"){
+  else if(fme == "ibfanart4mini.jpg"){
     expandimg.src = "ibfanart4.jpg";
   }
-  else if(images.src == "ibfanartmini5.jpg"){
+  else if(fme == "ibfanart5mini.jpg"){
+    console.log("here");
     expandimg.src = "ibfanart5.jpg";
   }
   expandedImg.style.display = "block";
